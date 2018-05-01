1afb16610af366fec368dd2c66652dec8e1b2445
Rafal Swieton <rafal@eskrzynka.pl>
2018-05-01 18:00:06 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '180472955792-sok0i7u698di3i6tlkjor9um3n7jhpah.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '5CYKXmTY2zaVw2t16GktCGfU',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
